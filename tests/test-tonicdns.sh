dd3787c21c3933f5145c5df7089f7e52fa4900f4
Nick Douma <n.douma@nekoconeko.nl>
2011-12-20 13:49:47 +0100

    Add Jenkins setup file

--- tests/test-tonicdns.sh
+++ tests/test-tonicdns.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-phpunit --coverage-html tonicdns/coverage --log-junit tonicdns/results/result.xml tonicdns/
+phpunit --coverage-html tonicdns/coverage --log-junit tonicdns/results/result.xml --coverage-clover tonicdns/coverage/coverage.xml tonicdns/
