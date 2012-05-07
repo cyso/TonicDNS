bcf666df7f09d9f4ac0bd4c29dcdc31292c17b97
Kouhei Maeda <mkouhei@palmtb.net>
2012-05-07 16:25:48 +0900

    Change type of username varchar(16) to varchar(50)
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- db/tables.sql
+++ db/tables.sql
@@ -73,7 +73,7 @@ CREATE TABLE IF NOT EXISTS `tokens` (
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(16) NOT NULL,
+  `username` varchar(50) NOT NULL,
   `password` varchar(34) NOT NULL,
   `fullname` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
